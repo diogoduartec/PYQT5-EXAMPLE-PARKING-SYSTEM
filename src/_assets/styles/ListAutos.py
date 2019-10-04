8cbc7668b060f3a815e4ea0c62d3ddb04e5b00d1
Diogo Roberto Duarte da Costa <drdc.snf17@dmz.org.br>
2019-10-04 14:48:38 -0400

    style: change padding on list autos

--- src/_assets/styles/ListAutos.py
+++ src/_assets/styles/ListAutos.py
@@ -2,4 +2,4 @@ import src._assets.Colors as COLORS
 import src._assets.styles.general as GENERAL
 
 button = GENERAL.button_dark
-list_autos = 'QListWidget{border: none} QListWidget::item{padding: 3} QListWidget::item:selected{background-color: '+COLORS.dark3+'}'
+list_autos = 'QListWidget{border: none} QListWidget::item{padding: 10} QListWidget::item:selected{background-color: '+COLORS.dark3+'}'
